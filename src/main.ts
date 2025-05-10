8e2388e9e9b36d0e78f3ef52cc7189688f6f291a
Infarcactus <mathieu.fournet04@orange.fr>
2025-05-10 22:55:19 +0200

    Formatting

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,28 @@
 import { invoke } from "@tauri-apps/api/core";
-import {handleSaveFileAndExtractToJson, SaveProcessResult} from "./SaveHandler.ts";
-
+import {
+  handleSaveFileAndExtractToJson,
+  SaveProcessResult,
+} from "./SaveHandler.ts";
 
 let SaveFileSelectEl: HTMLInputElement | null;
 
 let InfoBannerEl: HTMLDivElement | null;
 
-
-
 window.addEventListener("DOMContentLoaded", () => {
   SaveFileSelectEl = document.querySelector("#SaveFileSelect");
   InfoBannerEl = document.querySelector("#infoBanner");
   if (!SaveFileSelectEl || !InfoBannerEl) {
-    alert("Please check the html - some expected elements are not here")
+    alert("Please check the html - some expected elements are not here");
   }
   SaveFileSelectEl?.addEventListener("click", async (e) => {
-
-    handleSaveFileAndExtractToJson().then((saveProcessResult) => {
-      if (!saveProcessResult.success) InfoBannerEl!.innerText = saveProcessResult.message;
-      else InfoBannerEl!.innerText = `OK`;
-    }).catch((reason) => InfoBannerEl!.innerText = `Promise rejected. ${reason}`)
+    handleSaveFileAndExtractToJson()
+      .then((saveProcessResult) => {
+        if (!saveProcessResult.success)
+          InfoBannerEl!.innerText = saveProcessResult.message;
+        else InfoBannerEl!.innerText = `OK`;
+      })
+      .catch(
+        (reason) => (InfoBannerEl!.innerText = `Promise rejected. ${reason}`)
+      );
   });
 });
