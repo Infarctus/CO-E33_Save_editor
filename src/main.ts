979448281cbeeca95cc7a82a66a04c6e1d174792
autodistries <autodistries@>
2025-05-10 15:44:23 +0100

    Add info banner for showing status

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,24 @@
 import { invoke } from "@tauri-apps/api/core";
-import {handleSaveFileAndExtractToJson} from "./SaveHandler.ts";
+import {handleSaveFileAndExtractToJson, SaveProcessResult} from "./SaveHandler.ts";
 
 
 let SaveFileSelectEl: HTMLInputElement | null;
+
+let InfoBannerEl: HTMLDivElement | null;
 
 
 
 window.addEventListener("DOMContentLoaded", () => {
   SaveFileSelectEl = document.querySelector("#SaveFileSelect");
-  SaveFileSelectEl?.addEventListener("click", (e) => {
-    handleSaveFileAndExtractToJson();
+  InfoBannerEl = document.querySelector("#infoBanner");
+  if (!SaveFileSelectEl || !InfoBannerEl) {
+    alert("Please check the html - some expected elements are not here")
+  }
+  SaveFileSelectEl?.addEventListener("click", async (e) => {
+
+    handleSaveFileAndExtractToJson().then((saveProcessResult) => {
+      if (!saveProcessResult.success) InfoBannerEl!.innerText = saveProcessResult.message;
+      else InfoBannerEl!.innerText = `OK`;
+    }).catch((reason) => InfoBannerEl!.innerText = `Promise rejected. ${reason}`)
   });
 });
