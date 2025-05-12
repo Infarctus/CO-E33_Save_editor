6d9076bc9a899f48a7d0fc2f7334f8e9e7c74d15
autodistries <autodistries@>
2025-05-12 14:35:55 +0100

    complete mappings for characters

--- src/mappingjson/GeneralMappings.ts
+++ src/mappingjson/GeneralMappings.ts
@@ -3,6 +3,33 @@ export interface IntTag {
   tag: Tags;
 }
 
+export interface DoubleTag {
+  Double: number;
+  tag: Tags;
+}
+
+
+
+
+export interface BoolTag {
+  Bool: boolean;
+  tag: Tags;
+}
+
+export interface StringTag {
+  Name: string;
+  tag: Tags;
+}
+
+export interface StringsArrayTag {
+  tag: any;
+  Array: {
+    Base: {
+      Name: string[];
+    };
+  };
+}
+
 interface Tags {
   data: TagsData;
 }
