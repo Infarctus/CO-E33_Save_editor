78f4ec899148902e2c999a2be83f85ecb9bdb743
Infarcactus <149241321+Infarctus@users.noreply.github.com>
2025-05-13 23:00:31 +0200

    Added pictos

--- originalGameMapping/createcustommappings.py
+++ originalGameMapping/createcustommappings.py
@@ -33,15 +33,43 @@ def genpicto():
 
     # Write the output JSON
     with open(output_path, "w", encoding="utf-8") as f:
-        json.dump(output_data, f, indent=4)
+        json.dump(output_data, f, indent=2)
 
     print(f"Custom mappings written to {output_path}")
 
 def skinmapping():
-    input_skindefs = "originalGameMapping/DT_CharacterCustomizationItems.json"
+    input_cc = "originalGameMapping/DT_CharacterCustomizationItems.json"
 
     output_path = os.path.join(output_dir, "skins.json")
 
-    with open(input_skindefs, "r", encoding="utf-8") as f:
-        pictoslist = json.load(f)[0].get("Rows")
+    with open(input_cc, "r", encoding="utf-8") as f:
+        cclist = json.load(f)[0].get("Rows")
     
+
+    output_data = {
+        "Skins": {},
+        "Faces" : {}
+    }
+    for cc in cclist:
+        if cc.endswith("Default"):
+            continue
+        ccign =  cclist[cc].get("Item_DisplayName_89_41C0C54E4A55598869C84CA3B5B5DECA").get("SourceString")
+        isskin = True if cc.startswith("Skin") else False 
+        if isskin:
+            char = cc.removeprefix("Skin").split("_")[0]
+            if char not in output_data["Skins"]:
+                output_data["Skins"][char] = {}
+            output_data["Skins"][char][cc] = ccign
+        else:
+            char = cc.removeprefix("Face").split("_")[0]
+            if char not in output_data["Faces"]:
+                output_data["Faces"][char] = {}
+            output_data["Faces"][char][cc] = ccign
+
+    with open(output_path, "w", encoding="utf-8") as f:
+        json.dump(output_data, f, indent=2, ensure_ascii=False)
+
+
+
+genpicto()
+skinmapping()
