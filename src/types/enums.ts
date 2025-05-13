3baa1350824e7ebfe7f22b19cc3ed52d11f00aeb
autodistries <66792425+autodistries@users.noreply.github.com>
2025-05-13 20:21:09 +0100

    adapt the Attributes enum

--- src/types/enums.ts
+++ src/types/enums.ts
@@ -6,3 +6,9 @@ export enum ECharacterAttributeEnum {
   Luck = "Luck",
   Invalid = "Invalid",
 }
+
+
+
+export function getECharacterAttributeEnumValue(index: number): string {
+  return Object.entries(ECharacterAttributeEnum).find((_indx, value) =>  value == index)?.[0]  || "What is this shit";
+}
