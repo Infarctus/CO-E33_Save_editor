0f19f089cebfe015e97fa9c1b9418401a85f9bbc
Infarcactus <mathieu.fournet04@orange.fr>
2025-05-14 15:54:08 +0200

    Changed name of custom json mapping

---
+++ src/types/jsonCustomMapping.ts
@@ -0,0 +1,8 @@
+export interface CharCustomizationMapping {
+  Faces: any;
+  Skins: any;
+}
+
+export interface CustomPictosMapping {
+  Pictos: any;
+}
--- src/types/jsonSkinsCustomizationMapping.ts
+++
@@ -1,6 +0,0 @@
-
-
-export interface CustomizationMapping {
-    Faces: any
-    Skins: any;
-}
