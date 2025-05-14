48cc1108f9a497f1e582e891f400a128dd216db6
Infarcactus <mathieu.fournet04@orange.fr>
2025-05-14 21:46:21 +0200

    Changed picto logic + imported picto files

--- originalGameMapping/createcustommappings.py
+++ originalGameMapping/createcustommappings.py
@@ -3,7 +3,7 @@ import json
 
 output_dir = "./src-tauri/resources/customjsonmappings"
 os.makedirs(output_dir, exist_ok=True)
-def genpictomapping():
+def oldtestgenpictomapping():
     # Path to the input JSON file
     intput_pictosdefs = "originalGameMapping/DT_PictosDefinitions.json"
     input_pictosnames = "originalGameMapping/ST_PassiveEffects.json"
@@ -37,6 +37,94 @@ def genpictomapping():
         json.dump(output_data, f, indent=2)
 
     print("Picto mapping generated successfully.")
+
+def testpictoname(testpictodeffromfilename, GearPictosKey,pictosnamesdef):
+    if "CriticalBreak" in testpictodeffromfilename or "CriticalBreak" in GearPictosKey:
+        print("testpictodeffromfilename", testpictodeffromfilename)
+        print("GearPictosKey", GearPictosKey)
+    pictoname = pictosnamesdef.get("PASSIVE_"+GearPictosKey+"_Name")
+    if not pictoname:
+        pictoname = pictosnamesdef.get("PASSIVE_"+testpictodeffromfilename+"_Name")
+    if not pictoname:
+        pictoname = pictosnamesdef.get("PASSIVE_"+GearPictosKey.replace("+1","")+"_Name")
+    if not pictoname:
+        pictoname = pictosnamesdef.get("PASSIVE_"+testpictodeffromfilename.replace("_","")+"_Name")
+    if not pictoname:
+        pictoname = pictosnamesdef.get("PASSIVE_"+testpictodeffromfilename.replace("_2","B")+"_Name")
+    if not pictoname:
+        pictoname = pictosnamesdef.get("PASSIVE_"+testpictodeffromfilename.replace("_3","C")+"_Name")
+
+    if "CriticalBreak" in testpictodeffromfilename or "CriticalBreak" in GearPictosKey:
+        print(pictoname)
+    return pictoname
+
+def genpictomapping():
+    input_pictosnames = "originalGameMapping/ST_PassiveEffects.json"
+
+    with open(input_pictosnames, "r", encoding="utf-8") as f:
+        pictosnamesdef = json.load(f)[0].get("StringTable").get("KeysToEntries")
+
+    output_path = os.path.join(output_dir, "pictos.json")
+    output_data = {
+        "Pictos": {},
+        #"Unused": []
+    }
+    for filename in os.listdir("originalGameMapping/Gear"):
+        full_path = os.path.join("originalGameMapping/Gear", filename)
+        if not os.path.isfile(full_path):
+            print(f"Skipping {filename}, not a file.")
+            continue
+        if not filename.startswith("DA_Gear"):
+            continue
+        if filename.endswith(".json"):
+            with open(os.path.join("originalGameMapping/Gear", filename), "r", encoding="utf-8") as f:
+                passiveeffectname = json.load(f)[0].get("Properties").get("PassiveEffects")
+                if passiveeffectname:
+                    passiveeffectname = passiveeffectname[0].get("Row_2_8FD54A69441F52BAEE473DB8C91D1621").get("RowName")
+                    
+                if passiveeffectname:
+                    GearPictosKey = passiveeffectname
+                    testpictodeffromfilename = filename.removesuffix(".json").removeprefix("DA_GearPictos_")
+                    pictoname = testpictoname(testpictodeffromfilename, GearPictosKey, pictosnamesdef)
+                    if pictoname:
+                        output_data["Pictos"][GearPictosKey] = pictoname
+                    #else:
+                    #    output_data["Pictos"][GearPictosKey] = "NULL"
+
+    for filename in os.listdir("originalGameMapping/Gear/NewLumina"):
+        full_path = os.path.join("originalGameMapping/Gear/NewLumina", filename)
+        if not os.path.isfile(full_path):
+            continue
+        if not filename.startswith("DA_Gear"):
+            continue
+        if filename.endswith(".json"):
+            with open(os.path.join("originalGameMapping/Gear/NewLumina", filename), "r", encoding="utf-8") as f:
+                passiveeffectname = json.load(f)[0].get("Properties").get("PassiveEffects")
+                if passiveeffectname:
+                    passiveeffectname = passiveeffectname[0].get("Row_2_8FD54A69441F52BAEE473DB8C91D1621").get("RowName")
+                    
+                if passiveeffectname:
+                    GearPictosKey = passiveeffectname
+                    if GearPictosKey:
+                        testpictodeffromfilename = filename.removesuffix(".json").removeprefix("DA_GearPictos_")
+                        pictoname = testpictoname(testpictodeffromfilename, GearPictosKey, pictosnamesdef)
+                        if not pictoname:
+                            pictoname = pictosnamesdef.get("PASSIVE_"+GearPictosKey+"_Name")
+
+                        if pictoname:
+                            output_data["Pictos"][GearPictosKey] = pictoname
+                        #else:
+                        #    output_data["Pictos"][GearPictosKey] = "NULL"
+
+                           
+    with open(output_path, "w", encoding="utf-8") as f:
+        json.dump(output_data, f, indent=2)
+
+    print("Picto mapping generated successfully.")
+                    
+                
+
+
 
 def genskinmapping():
     input_cc = "originalGameMapping/DT_CharacterCustomizationItems.json"
