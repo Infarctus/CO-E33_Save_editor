09f2cc7e9733a48f9c5229d0561a811947eb1aa0
Infarcactus <mathieu.fournet04@orange.fr>
2025-05-16 18:37:35 +0200

    Formatted + removed useless import from InventoryPanel

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,63 @@
-"use client"
+"use client";
 
-import { useState } from "react"
-import { confirm } from "@tauri-apps/plugin-dialog"
-import Sidebar from "./components/Sidebar"
-import SaveFilePanel from "./components/SaveFilePanel"
-import CharactersPanel from "./components/CharactersPanel"
-import InventoryPanel from "./components/InventoryPanel"
-import BackupsPanel from "./components/BackupsPanel"
-import RawJsonPanel from "./components/RawJsonPanel"
-import InfoBanner from "./components/InfoBanner"
-import MusicDisksPanel from "./components/MusicDisksPanel"
-import JournalsPanel from "./components/JournalsPanel"
-import { handleSaveFileAndExtractToJson, handleJsonAndConvertToSaveFile } from "./utils/fileManagement"
-import { getMappingJsonFromFile, saveMappingJsonToDisk } from "./utils/jsonSaveMapping"
-import type { OpenProcessResult } from "./types/fileTypes"
-import type { BeginMapping } from "./types/jsonSaveMapping"
-import "./styles.css"
-import PictosPanel from "./components/PictosPanel"
-import { trace, error } from "@tauri-apps/plugin-log"
-import { useInfo } from "./components/InfoContext"
-import WeaponsPanel from "./components/WeaponsPanel"
-
-
-
-
-
-
+import { useState } from "react";
+import { confirm } from "@tauri-apps/plugin-dialog";
+import Sidebar from "./components/Sidebar";
+import SaveFilePanel from "./components/SaveFilePanel";
+import CharactersPanel from "./components/CharactersPanel";
+import BackupsPanel from "./components/BackupsPanel";
+import RawJsonPanel from "./components/RawJsonPanel";
+import InfoBanner from "./components/InfoBanner";
+import MusicDisksPanel from "./components/MusicDisksPanel";
+import JournalsPanel from "./components/JournalsPanel";
+import {
+  handleSaveFileAndExtractToJson,
+  handleJsonAndConvertToSaveFile,
+} from "./utils/fileManagement";
+import {
+  getMappingJsonFromFile,
+  saveMappingJsonToDisk,
+} from "./utils/jsonSaveMapping";
+import type { OpenProcessResult } from "./types/fileTypes";
+import type { BeginMapping } from "./types/jsonSaveMapping";
+import "./styles.css";
+import PictosPanel from "./components/PictosPanel";
+import { trace, error } from "@tauri-apps/plugin-log";
+import { useInfo } from "./components/InfoContext";
+import WeaponsPanel from "./components/WeaponsPanel";
 
 function App() {
-  const [activeTab, setActiveTab] = useState<string>("SaveFile")
-  const [workingFileCurrent, setWorkingFileCurrent] = useState<OpenProcessResult | null>(null)
-  const [saveNeeded, setSaveNeeded] = useState<boolean>(false)
-  const [jsonMapping, setJsonMapping] = useState<BeginMapping | null>(null)
+  const [activeTab, setActiveTab] = useState<string>("SaveFile");
+  const [workingFileCurrent, setWorkingFileCurrent] =
+    useState<OpenProcessResult | null>(null);
+  const [saveNeeded, setSaveNeeded] = useState<boolean>(false);
+  const [jsonMapping, setJsonMapping] = useState<BeginMapping | null>(null);
   // const [infoMessage, setInfoMessage] = useState<string>("Welcome. Use the Open File button to get started.")
-  const [jsonChangedSinceInit, setJsonChangedSinceInit] = useState(false)
-  
+  const [jsonChangedSinceInit, setJsonChangedSinceInit] = useState(false);
+
   const { infoMessage, setInfoMessage } = useInfo();
-function errorAndInfo(message: string) {
-  setInfoMessage(message)
-  error(message)
-}
+  function errorAndInfo(message: string) {
+    setInfoMessage(message);
+    error(message);
+  }
 
   const changedatesaved = () => {
     if (workingFileCurrent && workingFileCurrent.tempJsonPath && jsonMapping) {
       // Get .NET ticks (100-nanosecond intervals since 0001-01-01T00:00:00Z)
       const now = new Date();
       const epochTicks = 621355968000000000; // Number of .NET ticks at Unix epoch
-      const ticks = epochTicks + (now.getTime()-now.getTimezoneOffset() * 60000) * 10000;
+      const ticks =
+        epochTicks + (now.getTime() - now.getTimezoneOffset() * 60000) * 10000;
       try {
         jsonMapping.root.properties.SaveDateTime_0.Struct.DateTime = ticks;
+      } catch (error) {
+        errorAndInfo(
+          "Error setting SaveDateTime_0: (maybe not a CO:E33 Save file) " +
+            error
+        );
       }
-      catch (error) {
-        errorAndInfo("Error setting SaveDateTime_0: (maybe not a CO:E33 Save file) " + error);
-      }
-      
     }
-  }
-
+  };
 
   // Override console methods to capture logs
   // useConsoleOverride(setLogs, setInfoMessage)
@@ -68,114 +69,134 @@ function errorAndInfo(message: string) {
   //     trace("console debug")
   //   });
 
-
-
   //   initGameMappings();
   // }, []); // Empty dependency array ensures this runs only once
-
 
   const switchTab = async (tabName: string): Promise<boolean> => {
     // Check if we're leaving the RawJson tab with unsaved changes
     if (activeTab === "RawJson" && jsonChangedSinceInit) {
       if (
         !(await confirm(
-          "Clicking OK will DISCARD the changes made in the json editor.\nClick 'Cancel', then 'Commit Changes' to save them.",
+          "Clicking OK will DISCARD the changes made in the json editor.\nClick 'Cancel', then 'Commit Changes' to save them."
         ))
       ) {
-        return false
+        return false;
       }
     }
 
-    setActiveTab(tabName)
-    trace(`${tabName} Tab Activated`)
-    return true
-  }
+    setActiveTab(tabName);
+    trace(`${tabName} Tab Activated`);
+    return true;
+  };
 
   const updateNavStates = (anyFileOpen: boolean) => {
-    trace("Currently any file open:"+ anyFileOpen)
+    trace("Currently any file open:" + anyFileOpen);
     // This function now just updates the state
     // The UI components will use this state to determine if they should be disabled
-  }
+  };
 
   const triggerSaveNeeded = () => {
-    setSaveNeeded(true)
-  }
+    setSaveNeeded(true);
+  };
 
   const handleOpenFile = async () => {
-    if ((await switchTab("SaveFile")) === false) return
+    if ((await switchTab("SaveFile")) === false) return;
 
     if (workingFileCurrent != null && saveNeeded) {
       if (
         !(await confirm(
-          "Clicking OK will DISCARD the changes made to the file you're currently editing.\nSave it before opening another one if needed.",
+          "Clicking OK will DISCARD the changes made to the file you're currently editing.\nSave it before opening another one if needed."
         ))
       ) {
-        return
+        return;
       }
-      setSaveNeeded(false)
+      setSaveNeeded(false);
     }
 
-    setWorkingFileCurrent(null)
-    updateNavStates(false)
+    setWorkingFileCurrent(null);
+    updateNavStates(false);
 
-    const saveProcessResult = await handleSaveFileAndExtractToJson()
+    const saveProcessResult = await handleSaveFileAndExtractToJson();
     if (saveProcessResult.success) {
-      setWorkingFileCurrent(saveProcessResult)
-      setInfoMessage(saveProcessResult.message)
-      trace("Opened save OK: " + saveProcessResult.message)
-      updateNavStates(true)
+      setWorkingFileCurrent(saveProcessResult);
+      setInfoMessage(saveProcessResult.message);
+      trace("Opened save OK: " + saveProcessResult.message);
+      updateNavStates(true);
 
       // Load the JSON mapping
       if (saveProcessResult.tempJsonPath) {
-        const mapping = await getMappingJsonFromFile(saveProcessResult.tempJsonPath)
-        setJsonMapping(mapping)
+        const mapping = await getMappingJsonFromFile(
+          saveProcessResult.tempJsonPath
+        );
+        setJsonMapping(mapping);
       }
     } else {
-      errorAndInfo(saveProcessResult.message)
+      errorAndInfo(saveProcessResult.message);
     }
-  }
+  };
 
   const handleExportFile = async () => {
     // Ensure we have a workingFileCurrent with a tempJsonPath
-    if (!workingFileCurrent || !workingFileCurrent.tempJsonPath || !jsonMapping) {
-      errorAndInfo("No working file (temp JSON path) available.")
+    if (
+      !workingFileCurrent ||
+      !workingFileCurrent.tempJsonPath ||
+      !jsonMapping
+    ) {
+      errorAndInfo("No working file (temp JSON path) available.");
 
-      return
+      return;
     }
-    changedatesaved()
+    changedatesaved();
 
     try {
       // First, save the JSON mapping to the working temp path
-      await saveMappingJsonToDisk(workingFileCurrent.tempJsonPath, jsonMapping)
+      await saveMappingJsonToDisk(workingFileCurrent.tempJsonPath, jsonMapping);
 
       // Ask user where to save the save file (either .sav or no extension)
-      const currentFileExt = workingFileCurrent.originalSavPath?.lastIndexOf(".") ===-1 ? "*" : workingFileCurrent.originalSavPath?.split(".")[1]
-      trace(`${workingFileCurrent.originalSavPath} is the og file path (${currentFileExt}) ${workingFileCurrent.originalSavPath?.split(".")}`)
-      const { save } = await import("@tauri-apps/plugin-dialog")
+      const currentFileExt =
+        workingFileCurrent.originalSavPath?.lastIndexOf(".") === -1
+          ? "*"
+          : workingFileCurrent.originalSavPath?.split(".")[1];
+      trace(
+        `${
+          workingFileCurrent.originalSavPath
+        } is the og file path (${currentFileExt}) ${workingFileCurrent.originalSavPath?.split(
+          "."
+        )}`
+      );
+      const { save } = await import("@tauri-apps/plugin-dialog");
       const targetSavPath = await save({
         title: "Select the destination for the exported save file",
-        filters: [{ name: `${currentFileExt} File`, extensions: [currentFileExt ? currentFileExt : "*"] }],
-      })
+        filters: [
+          {
+            name: `${currentFileExt} File`,
+            extensions: [currentFileExt ? currentFileExt : "*"],
+          },
+        ],
+      });
 
       if (!targetSavPath) {
-        errorAndInfo("Export canceled or no target SAV path selected.")
-        return
+        errorAndInfo("Export canceled or no target SAV path selected.");
+        return;
       }
 
       // Now, call the conversion function
-      const result = await handleJsonAndConvertToSaveFile(workingFileCurrent.tempJsonPath, targetSavPath)
+      const result = await handleJsonAndConvertToSaveFile(
+        workingFileCurrent.tempJsonPath,
+        targetSavPath
+      );
       if (result.success) {
-      setInfoMessage(result.message)
+        setInfoMessage(result.message);
 
-        trace(result.message)
-        setSaveNeeded(false)
+        trace(result.message);
+        setSaveNeeded(false);
       } else {
-        errorAndInfo(result.message)
+        errorAndInfo(result.message);
       }
     } catch (err) {
-      errorAndInfo("Error during export process:"+ err)
+      errorAndInfo("Error during export process:" + err);
     }
-  }
+  };
 
   const handleOverwriteFile = async () => {
     // Ensure we have a workingFileCurrent with an originalSavPath
@@ -185,45 +206,50 @@ function errorAndInfo(message: string) {
       !workingFileCurrent.tempJsonPath ||
       !jsonMapping
     ) {
-      errorAndInfo("No working file (original SAV path or temp JSON path) available.")
-      return
+      errorAndInfo(
+        "No working file (original SAV path or temp JSON path) available."
+      );
+      return;
     }
-    changedatesaved()
+    changedatesaved();
 
     try {
       // Save JSON to the temp file
-      await saveMappingJsonToDisk(workingFileCurrent.tempJsonPath, jsonMapping)
+      await saveMappingJsonToDisk(workingFileCurrent.tempJsonPath, jsonMapping);
 
       // For overwrite simply use the original save file path
-      const targetSavPath = workingFileCurrent.originalSavPath
+      const targetSavPath = workingFileCurrent.originalSavPath;
 
       // Call the conversion function
-      const result = await handleJsonAndConvertToSaveFile(workingFileCurrent.tempJsonPath, targetSavPath)
+      const result = await handleJsonAndConvertToSaveFile(
+        workingFileCurrent.tempJsonPath,
+        targetSavPath
+      );
       if (result.success) {
-        trace(result.message)
-      setInfoMessage(result.message)
+        trace(result.message);
+        setInfoMessage(result.message);
 
-        setSaveNeeded(false)
+        setSaveNeeded(false);
       } else {
-        errorAndInfo(result.message)
+        errorAndInfo(result.message);
       }
     } catch (err) {
-      errorAndInfo("Error during overwrite process:"+ err)
+      errorAndInfo("Error during overwrite process:" + err);
     }
-  }
+  };
 
   const handleJsonChange = () => {
-    setJsonChangedSinceInit(true)
-  }
+    setJsonChangedSinceInit(true);
+  };
 
   const commitJsonChanges = (jsonData: BeginMapping) => {
     if (workingFileCurrent != null) {
-      setJsonChangedSinceInit(false)
-      triggerSaveNeeded()
-      setJsonMapping(jsonData)
-      trace("Committed raw json changes")
+      setJsonChangedSinceInit(false);
+      triggerSaveNeeded();
+      setJsonMapping(jsonData);
+      trace("Committed raw json changes");
     }
-  }
+  };
 
   return (
     <div className="layout">
@@ -264,8 +290,7 @@ function errorAndInfo(message: string) {
             jsonMapping={jsonMapping}
             triggerSaveNeeded={triggerSaveNeeded}
           />
-)}
-
+        )}
 
         {activeTab === "Weapons" && (
           <WeaponsPanel
@@ -273,19 +298,20 @@ function errorAndInfo(message: string) {
             triggerSaveNeeded={triggerSaveNeeded}
           />
         )}
-        {activeTab === "Backups" && (
-          <BackupsPanel />
-        )}
+        {activeTab === "Backups" && <BackupsPanel />}
 
         {activeTab === "RawJson" && (
-          <RawJsonPanel jsonMapping={jsonMapping} onJsonChange={handleJsonChange} onCommitChanges={commitJsonChanges} />
+          <RawJsonPanel
+            jsonMapping={jsonMapping}
+            onJsonChange={handleJsonChange}
+            onCommitChanges={commitJsonChanges}
+          />
         )}
       </main>
 
       <InfoBanner message={infoMessage} />
     </div>
-  )
+  );
 }
 
-export default App
-
+export default App;
