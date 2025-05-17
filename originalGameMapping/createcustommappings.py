362a205ad0dc9fb8b59143baa4ee0d60875c7217
Infarcactus <149241321+Infarctus@users.noreply.github.com>
2025-05-17 23:19:52 +0200

    New mappings :D

--- originalGameMapping/createcustommappings.py
+++ originalGameMapping/createcustommappings.py
@@ -10,150 +10,64 @@ def jsondump(obj, file):
         json.dump(obj, f, separators=(',', ':'), ensure_ascii=False) # for release to minify the jsons
 
 
-def oldtestgenpictomapping():
-    # Path to the input JSON file
-    intput_pictosdefs = "originalGameMapping/DT_PictosDefinitions.json"
-    input_pictosnames = "originalGameMapping/ST_PassiveEffects.json"
+itemtypes = {'E_jRPG_ItemType::NewEnumerator0': 'Weapon', 'E_jRPG_ItemType::NewEnumerator6': 'N/A', 'E_jRPG_ItemType::NewEnumerator7': 'Consumable', 'E_jRPG_ItemType::NewEnumerator10': 'Pictos', 'E_jRPG_ItemType::NewEnumerator11': 'Key', 'E_jRPG_ItemType::NewEnumerator12': 'Inventory', 'E_jRPG_ItemType::NewEnumerator14': 'Shard', 'E_jRPG_ItemType::NewEnumerator15': 'Gold', 'E_jRPG_ItemType::NewEnumerator16': 'CharacterCustomization', 'E_jRPG_ItemType::NewEnumerator17': 'SkillUnlocker'}
+itemsubtypes = {'E_jRPG_ItemSubtype::NewEnumerator0': 'Lune', 'E_jRPG_ItemSubtype::NewEnumerator1': 'Monoco', 'E_jRPG_ItemSubtype::NewEnumerator2': 'Sciel', 'E_jRPG_ItemSubtype::NewEnumerator11': 'Consumable', 'E_jRPG_ItemSubtype::NewEnumerator14': 'Maelle', 'E_jRPG_ItemSubtype::NewEnumerator15': 'Pictos', 'E_jRPG_ItemSubtype::NewEnumerator16': 'Noah', 'E_jRPG_ItemSubtype::NewEnumerator18': 'Key', 'E_jRPG_ItemSubtype::NewEnumerator19': 'Inventory', 'E_jRPG_ItemSubtype::NewEnumerator20': 'Invalid', 'E_jRPG_ItemSubtype::NewEnumerator21': 'Verso', 'E_jRPG_ItemSubtype::NewEnumerator22': 'Journal', 'E_jRPG_ItemSubtype::NewEnumerator23': 'Music Record'}
 
-    # Path to the output directory and file
-    output_path = os.path.join(output_dir, "pictos.json")
-
-    # Load the input JSON
-    with open(intput_pictosdefs, "r", encoding="utf-8") as f:
-        pictoslist = json.load(f)[0].get("Rows")
-
-
-    with open(input_pictosnames, "r", encoding="utf-8") as f:
-        pictosnamesdef = json.load(f)[0].get("StringTable").get("KeysToEntries")
-
-    # Prepare the output JSON structure
-    output_data = {
-        "Pictos": {},
-        #"Unused": []
-    }
-    for picto in pictoslist:
-        pictoname = pictosnamesdef.get("PASSIVE_"+picto+"_Name")
-        if pictoname:
-            output_data["Pictos"][picto] = pictoname
-        else:
-            output_data["Pictos"][picto] = "NULL"
-            #output_data["Unused"].append(picto)
-
-    # Write the output JSON
-    jsondump(output_data, output_path)
-
-    print("Picto mapping generated successfully.")
-
-def testpictoname(testpictodeffromfilename, GearPictosKey,pictosnamesdef):
-    # if "CriticalBreak" in testpictodeffromfilename or "CriticalBreak" in GearPictosKey:
-    #     print("testpictodeffromfilename", testpictodeffromfilename)
-    #     print("GearPictosKey", GearPictosKey)
-    pictoname = pictosnamesdef.get("PASSIVE_"+GearPictosKey+"_Name")
-    if not pictoname:
-        pictoname = pictosnamesdef.get("PASSIVE_"+testpictodeffromfilename+"_Name")
-    if not pictoname:
-        pictoname = pictosnamesdef.get("PASSIVE_"+GearPictosKey.replace("+1","")+"_Name")
-    if not pictoname:
-        pictoname = pictosnamesdef.get("PASSIVE_"+testpictodeffromfilename.replace("_","")+"_Name")
-    if not pictoname:
-        pictoname = pictosnamesdef.get("PASSIVE_"+testpictodeffromfilename.replace("_2","B")+"_Name")
-    if not pictoname:
-        pictoname = pictosnamesdef.get("PASSIVE_"+testpictodeffromfilename.replace("_3","C")+"_Name")
-
-    # if "CriticalBreak" in testpictodeffromfilename or "CriticalBreak" in GearPictosKey:
-    #     print(pictoname)
-    return pictoname
 
 def genpictomapping():
-    input_pictosnames = "originalGameMapping/ST_PassiveEffects.json"
+    items = "originalGameMapping/DT_jRPG_Items_Composite.json"
 
-    with open(input_pictosnames, "r", encoding="utf-8") as f:
-        pictosnamesdef = json.load(f)[0].get("StringTable").get("KeysToEntries")
+    with open(items, "r", encoding="utf-8") as f:
+        itemsdef = json.load(f)[0].get("Rows")
 
     output_path = os.path.join(output_dir, "pictos.json")
     output_data = {
-        "Pictos": {},
-        #"Unused": []
+        "Pictos": {}
     }
-    for filename in os.listdir("originalGameMapping/Gear"):
-        full_path = os.path.join("originalGameMapping/Gear", filename)
-        if not os.path.isfile(full_path):
-            continue
-        if not filename.startswith("DA_Gear"):
-            continue
-        if filename.endswith(".json"):
-            with open(os.path.join("originalGameMapping/Gear", filename), "r", encoding="utf-8") as f:
-                passiveeffectname = json.load(f)[0].get("Properties").get("PassiveEffects")
-                if passiveeffectname:
-                    passiveeffectname = passiveeffectname[0].get("Row_2_8FD54A69441F52BAEE473DB8C91D1621").get("RowName")
-                    
-                if passiveeffectname:
-                    GearPictosKey = passiveeffectname
-                    testpictodeffromfilename = filename.removesuffix(".json").removeprefix("DA_GearPictos_")
-                    pictoname = testpictoname(testpictodeffromfilename, GearPictosKey, pictosnamesdef)
-                    if pictoname:
-                        output_data["Pictos"][GearPictosKey] = pictoname
-                    #else:
-                    #    output_data["Pictos"][GearPictosKey] = "NULL"
 
-    for filename in os.listdir("originalGameMapping/Gear/NewLumina"):
-        full_path = os.path.join("originalGameMapping/Gear/NewLumina", filename)
-        if not os.path.isfile(full_path):
+    for item in itemsdef:
+        curritem = itemsdef[item]
+        if curritem.get("Item_Type_88_2F24F8FB4235429B4DE1399DBA533C78") != 'E_jRPG_ItemType::NewEnumerator10':
             continue
-        if not filename.startswith("DA_Gear"):
-            continue
-        if filename.endswith(".json"):
-            with open(os.path.join("originalGameMapping/Gear/NewLumina", filename), "r", encoding="utf-8") as f:
-                passiveeffectname = json.load(f)[0].get("Properties").get("PassiveEffects")
-                if passiveeffectname:
-                    passiveeffectname = passiveeffectname[0].get("Row_2_8FD54A69441F52BAEE473DB8C91D1621").get("RowName")
-                    
-                if passiveeffectname:
-                    GearPictosKey = passiveeffectname
-                    if GearPictosKey:
-                        testpictodeffromfilename = filename.removesuffix(".json").removeprefix("DA_GearPictos_")
-                        pictoname = testpictoname(testpictodeffromfilename, GearPictosKey, pictosnamesdef)
-                        if not pictoname:
-                            pictoname = pictosnamesdef.get("PASSIVE_"+GearPictosKey+"_Name")
+        pictoname = curritem.get("Item_DisplayName_89_41C0C54E4A55598869C84CA3B5B5DECA").get("SourceString")
+        if pictoname == None:
+            pictoname = curritem.get("Item_DisplayName_89_41C0C54E4A55598869C84CA3B5B5DECA").get("CultureInvariantString")+"**"
+        
+        output_data["Pictos"][item] = pictoname
+    
 
-                        if pictoname:
-                            output_data["Pictos"][GearPictosKey] = pictoname
-                        #else:
-                        #    output_data["Pictos"][GearPictosKey] = "NULL"
-
-                           
     jsondump(output_data, output_path)
 
     print("Picto mapping generated successfully.")              
                 
 def genskinmapping():
-    input_cc = "originalGameMapping/DT_CharacterCustomizationItems.json"
+    items = "originalGameMapping/DT_jRPG_Items_Composite.json"
+
+    disabledchar = ["Sophie","AliciaEpilogue","Alicia"]
 
     output_path = os.path.join(output_dir, "skins.json")
-
-    with open(input_cc, "r", encoding="utf-8") as f:
-        cclist = json.load(f)[0].get("Rows")
     
+    with open(items, "r", encoding="utf-8") as f:
+        itemsdef = json.load(f)[0].get("Rows")
 
     output_data = {
         "Skins": {},
         "Faces" : {}
     }
-    for cc in cclist:
-        if cc.endswith("Default"):
+    
+    for item in itemsdef:
+        curritem = itemsdef[item]
+        if curritem.get("Item_Type_88_2F24F8FB4235429B4DE1399DBA533C78") != 'E_jRPG_ItemType::NewEnumerator16':
             continue
-        ccign =  cclist[cc].get("Item_DisplayName_89_41C0C54E4A55598869C84CA3B5B5DECA").get("SourceString")
-        isskin = True if cc.startswith("Skin") else False 
-        if isskin:
-            char = cc.removeprefix("Skin").split("_")[0]
-            if char not in output_data["Skins"]:
-                output_data["Skins"][char] = {}
-            output_data["Skins"][char][cc] = ccign
-        else:
-            char = cc.removeprefix("Face").split("_")[0]
-            if char not in output_data["Faces"]:
-                output_data["Faces"][char] = {}
-            output_data["Faces"][char][cc] = ccign
+        charname = item.removeprefix("Skin").split("_")[0]
+        type = "Skins" if item.startswith("Skin") else "Faces"
+        charname = item.removeprefix(type[:-1]).split("_")[0]
+        if charname in disabledchar:
+            continue
+        if charname not in output_data[type]:
+            output_data[type][charname] = {}
+        skinname = curritem.get("Item_DisplayName_89_41C0C54E4A55598869C84CA3B5B5DECA").get("SourceString")
+        output_data[type][charname][item] = skinname
 
     jsondump(output_data, output_path)
 
@@ -178,46 +92,40 @@ def genmusicdiskmapping():
 
     print("Music disk mapping generated successfully.")
 
-
 def genweaponmapping():
-    input_weapons = "originalGameMapping/Gear/Weapons/DT_WeaponDefinitions.json"
+    items = "originalGameMapping/DT_jRPG_Items_Composite.json"
     output_path = os.path.join(output_dir, "weapons.json")
 
-    with open(input_weapons, "r", encoding="utf-8") as f:
-        weaponslistdef = json.load(f)[0].get("Rows")
+    with open(items, "r", encoding="utf-8") as f:
+        itemsdef = json.load(f)[0].get("Rows")
 
-    with open ("originalGameMapping/ST_Items.json", "r", encoding="utf-8") as f:
-        weaponslistnames = json.load(f)[0].get("StringTable").get("KeysToEntries")
+    #with open ("originalGameMapping/ST_Items.json", "r", encoding="utf-8") as f:
+    #    STItems = json.load(f)[0].get("StringTable").get("KeysToEntries")
 
     output_data = {
         "Weapons": {}
     }
 
-    for dir in os.listdir("originalGameMapping/Gear/Weapons"):
-        if dir.endswith(".json"):
+    for item in itemsdef:
+        curritem = itemsdef[item]
+        if curritem.get("Item_Type_88_2F24F8FB4235429B4DE1399DBA533C78") != 'E_jRPG_ItemType::NewEnumerator0':
             continue
-        charname = dir
-        diccharname = charname if charname != "Verso" else "Gustave/Verso" 
-        output_data["Weapons"][diccharname] = {}
-        full_path = os.path.join("originalGameMapping/Gear/Weapons", dir)
-        for weapon in os.listdir(full_path):
-            if weapon.endswith(".json"):
-                weaponname = weapon.removeprefix(weapon.split(charname)[0]+charname+"_").removesuffix(".json")
-                if(len(weaponname.split("_")[0]) ==2 ):
-                    weaponname = weaponname.removeprefix(weaponname.split("_")[0]+"_")
-                if (weaponname == "GDC"): # weird exception case
-                    continue
-                if( weaponslistdef.get(weaponname) ):
-                    itemlistname = weaponslistnames.get("ITEM_"+weaponname+"_Name")
-                    if not itemlistname:
-                        itemlistname = "NULL"
-                    output_data["Weapons"][diccharname][weaponname] = itemlistname
+        
+        charname = itemsubtypes[curritem.get("Item_Subtype_87_0CE0028F4D632385B61EDABBFBDF5360")]
+        charname= charname if charname != "Noah" else "Gustave" 
+
+        if charname not in output_data["Weapons"]:
+            output_data["Weapons"][charname] = {}
+        weaponname = curritem.get("Item_DisplayName_89_41C0C54E4A55598869C84CA3B5B5DECA").get("SourceString")
+        if weaponname == None:
+            weaponname = curritem.get("Item_DisplayName_89_41C0C54E4A55598869C84CA3B5B5DECA").get("CultureInvariantString")
+        output_data["Weapons"][charname][item] = weaponname
+    
 
     jsondump(output_data, output_path)
 
     print("Weapon mapping generated successfully.")
 
-        
 def genjournalsmapping():
     input_journals = "originalGameMapping/DT_Items_Journals.json"
     output_path = os.path.join(output_dir, "journals.json")
@@ -238,9 +146,10 @@ def genjournalsmapping():
 
     print("Journal mapping generated successfully.")
 
-    
-genjournalsmapping()
-genweaponmapping()
-genpictomapping()
-genskinmapping()
-genmusicdiskmapping()
+
+
+#genweaponmapping()    
+#genjournalsmapping()
+#genpictomapping()
+#genskinmapping()
+#genmusicdiskmapping()
