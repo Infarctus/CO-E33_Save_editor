93cc92effac4799369451cda831e7154c67cd375
Infarcactus <149241321+Infarctus@users.noreply.github.com>
2025-05-27 12:27:54 +0200

    Converted every input element to use the function for it

---
+++ src/utils/HtmlElement.tsx
@@ -0,0 +1,35 @@
+import { clamp } from "./utils"
+
+
+export function renderNumberInput(
+  value: number,
+  label: string,
+  minInput: number,
+  maxInput: number,
+  onChange: (newValue: number) => void,
+  disabled?: boolean,
+  StylemarginBottom = "0",
+) {
+  return (
+    <div style={{ marginBottom: StylemarginBottom }}>
+      {label !== "" && (
+      <label htmlFor={label.toLowerCase() + '-input'} style={{ marginRight: '0.5rem' }}>
+        {label}
+      </label>
+      )}
+      <input
+      id={label.toLowerCase() + '-input'}
+      type='number'
+      min={minInput}
+      max={maxInput}
+      value={value}
+      disabled={disabled}
+      onInput={(e) => {
+        const target = e.target as HTMLInputElement
+        onChange(clamp(target.valueAsNumber, minInput, maxInput) || 0)
+      }}
+      style={{ width: 'auto' }}
+      />
+    </div>
+  )
+}
