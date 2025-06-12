9fa74bb3cfb7ed9990f1c8d169fa94d226a8223f
autodistries <66792425+autodistries@users.noreply.github.com>
2025-06-12 18:44:02 +0200

    add search bar on locations panel

--- src/components/Panels/SpawnLocationPanel.tsx
+++ src/components/Panels/SpawnLocationPanel.tsx
@@ -1,4 +1,4 @@
-import { FC, useMemo, useState } from 'react'
+import { FC, useEffect, useMemo, useState } from 'react'
 import { GeneralPanelProps } from '../../types/panelTypes'
 import { getPossibleFlags } from '../../utils/gameMappingProvider'
 import { trace } from '@tauri-apps/plugin-log'
@@ -16,11 +16,18 @@ const SpawnLocationPanel: FC<GeneralPanelProps> = ({ jsonMapping, triggerSaveNee
   }
 
   const Flags = useMemo(() => getPossibleFlags(), [])
+  const [searchString, setSearchString] = useState<string>('')
+  const [shownLocations, setShownLocations] = useState(Flags)
 
   const [currlevel, setCurrLevel] = useState(jsonMapping.root.properties.MapToLoad_0.Name)
   const [currflag, setCurrFlag] = useState(
     jsonMapping.root.properties.SpawnPointTagToLoadAt_0.Struct.Struct.TagName_0.Name,
   )
+useEffect(() => {
+  setShownLocations(Object.fromEntries(
+    Object.entries(Flags).filter((el) => el[0].toLowerCase().includes(searchString.toLowerCase()))
+  ));
+}, [searchString]);
 
   const handleFlagClick = (levelKey: string, flagKey: string) => {
     setCurrLevel(levelKey)
@@ -31,8 +38,8 @@ const SpawnLocationPanel: FC<GeneralPanelProps> = ({ jsonMapping, triggerSaveNee
     triggerSaveNeeded()
   }
 
-  const renderLocationTable = () => {
-    return Object.entries(Flags).map(([locationName, locationData]) => {
+  const renderLocationTable = (searchString: string) => {
+    return Object.entries(shownLocations).map(([locationName, locationData]) => {
       const flags = [
         { key: locationData.MainSpawnPoint, name: 'Main', isMain: true },
         ...Object.entries(locationData.SubFlags).map(([key, flagname]) => ({
@@ -71,7 +78,16 @@ const SpawnLocationPanel: FC<GeneralPanelProps> = ({ jsonMapping, triggerSaveNee
         This panel allows you to change the spawn location of your character in the game. <br />
         It is really wonky, so be careful when using it. And some flags dont even exist in the game.
       </p>
-      <div className='spawn-location-flags'>{renderLocationTable()}</div>
+            <input
+        type='text'
+        placeholder='Search locations by name...'
+        className='search-bar'
+        value={searchString}
+        onChange={(e) => setSearchString(e.target.value)}
+      />
+      <br />
+      <br />
+      <div className='spawn-location-flags'>{renderLocationTable(searchString)}</div>
     </div>
   )
 }
